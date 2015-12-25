4b6078a77550af351e566aa4b0646e0184316e3c
Nickchooshin <gustncjswp95@gmail.com>
2015-12-25 09:19:53 +0900

    added GaugeBar and Thief Move script

---
+++ Assets/Resources/Scripts/Thief.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Thief : MonoBehaviour {
+
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(h * speed * Time.deltaTime, v * speed * Time.deltaTime);
+        transform.position += moveVector;
+    }
+}
